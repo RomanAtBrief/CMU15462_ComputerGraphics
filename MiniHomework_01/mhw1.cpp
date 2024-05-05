d39af349166f03fbabb2d08c5c9b7f342834aa05
Roman Gorshkov <roman@thebrief.space>
2024-05-05 12:34:17 +0100

    Using Eigen to solve linear system

--- MiniHomework_01/mhw1.cpp
+++ MiniHomework_01/mhw1.cpp
@@ -1,22 +1,38 @@
 /*
  * File: mhw1.cpp
  * ---------------------
- * Description
+ * This program solves a linear system with three
+ * equations and three unknowns.
+ *
+ * 1.2x + 3.4y + 5.6z = 36.4
+ * 7.8x + 9.0y + 1.2z = 87.6
+ * 3.4x + 5.6y + 7.8z = 62.8
+ *
  */
 
 /* Preprocessor statement always satrts with # */
 #include <iostream>
-#include "Eigen/Dense"
-
-using Eigen::MatrixXd;
+#include <Eigen/Dense>
 
 /* Main program */
 int main()
 {
-    MatrixXd m(2, 2);
-    m(0, 0) = 3;
-    m(1, 0) = 2.5;
-    m(0, 1) = -1;
-    m(1, 1) = m(1, 0) + m(0, 1);
-    std::cout << m << std::endl;
+    Eigen::Matrix3f A;
+    Eigen::Vector3f b;
+
+    // Matrix. Converting system of linear equations to matrix form
+    A << 1.2, 3.4, 5.6, 7.8, 9.0, 1.2, 3.4, 5.6, 7.8;
+    // Representing constants as a vector b
+    b << 36.4, 87.6, 62.8;
+
+    // Console information
+    std::cout << "The matrix on linear equation A is:\n"
+              << A << std::endl;
+    std::cout << "Constants as vector b:\n"
+              << b << std::endl;
+
+    // Solving equation and display solution
+    Eigen::Vector3f v = A.colPivHouseholderQr().solve(b);
+    std::cout << "The solution is: \n"
+              << v << std::endl;
 }
